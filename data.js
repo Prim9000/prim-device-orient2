38b29f0c0909b5d7d6b95edc384264e1fbd7affe
PrimWong <65888725+Prim9000@users.noreply.github.com>
2025-03-11 23:22:22 +0700

    original 2

---
+++ data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-forest",
+      "name": "Forest",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43267131887797206,
+          "pitch": 0.1645117160439593,
+          "rotation": 0,
+          "target": "1-water-spring"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-water-spring",
+      "name": "Water Spring",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6291562914762725,
+          "pitch": 0.24779904979965117,
+          "rotation": 0,
+          "target": "0-forest"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Prim 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
